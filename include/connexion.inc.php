50b41f7c6292fa66dbe1e54332cabd3cf7342002
Bertrand <bertrand.fernandez@gmail.com>
2017-09-06 15:26:59 +0200

    Ajout du fichier de connexion à la base de données

---
+++ include/connexion.inc.php
@@ -0,0 +1,10 @@
+<?php
+try {
+	$bdd = new PDO('mysql:host=localhost;dbname=aidedevoir;charset=utf8','root','');
+}
+catch( PDOException $Exception ) {
+    // Note The Typecast To An Integer!
+	throw new MyDatabaseException( $Exception->getMessage( ) , (int)$Exception->getCode( ) );
+}
+
+?>
